536c09febfe2e8d67fee4b459e9d4c0702b014bf
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-05-17 13:24:33 +0200

    Refactored the html
    Changed the Html back and put it into the js file

--- DUWFRI353_WFC2401_GroupA_DuwayneFrieslaar_DJS04-main/Web component.js
+++ DUWFRI353_WFC2401_GroupA_DuwayneFrieslaar_DJS04-main/Web component.js
@@ -1,10 +1,46 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
-import "./scripts.js"
+import "./scripts.js";
+class CustomHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        /* Include styles here */
+      </style>
+      <header class="header">
+        <div class="header__inner">
+          <div class="header__logo">
+            <svg class="header__shape" viewBox="0 0 89 68" xmlns="http://www.w3.org/2000/svg">
+              <!-- SVG code for logo shape -->
+            </svg>
+            <svg class="header__text" viewBox="0 0 652 74" xmlns="http://www.w3.org/2000/svg">
+              <!-- SVG code for logo text -->
+            </svg>
+          </div>
+          <div>
+            <button class="header__button" data-header-search>
+              <svg class="header__icon" viewBox="0 96 960 960" xmlns="http://www.w3.org/2000/svg">
+                <!-- SVG code for search icon -->
+              </svg>
+            </button>
+            <button class="header__button" data-header-settings>
+              <svg class="header__icon" viewBox="0 0 960 960" xmlns="http://www.w3.org/2000/svg">
+                <!-- SVG code for settings icon -->
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
+    `;
+  }
 
-// Importing data and constants from data.js file
+  // Add event listeners or additional functionality as needed
+}
+
+customElements.define('custom-header', CustomHeader);
 
 
-// Class for creating HTML elements with specified attributes and innerHTML
 class ElementCreator {
   createElement(tag, attributes, innerHTML) {
     const element = document.createElement(tag);
@@ -13,22 +49,25 @@ class ElementCreator {
     );
     element.innerHTML = innerHTML;
     return element;
-  }
-}
+  }}
+
 
 // Class for rendering dropdown options based on provided data
 class OptionsRenderer {
   render(data, selector, defaultValue) {
     const fragment = document.createDocumentFragment();
     fragment.appendChild(
-      new ElementCreator().createElement("option", { value: "any" }, defaultValue)
-    );
+      new ElementCreator().createElement(
+        "option",
+        { value: "any" },
+        defaultValue
+      ));
     Object.entries(data).forEach(([id, name]) =>
-      fragment.appendChild(new ElementCreator().createElement("option", { value: id }, name))
-    );
+      fragment.appendChild(
+        new ElementCreator().createElement("option", { value: id }, name)
+      ));
     document.querySelector(selector).appendChild(fragment);
-  }
-}
+  }}
 
 // Class for rendering books with preview information
 class BooksRenderer {
@@ -47,8 +86,8 @@ class BooksRenderer {
       fragment.appendChild(element);
     });
     document.querySelector("[data-list-items]").appendChild(fragment);
-  }
-}
+  }}
+
 
 // Initial rendering of books and dropdown options
 const elementCreator = new ElementCreator();
@@ -57,5 +96,3 @@ const booksRenderer = new BooksRenderer();
 booksRenderer.render(books, BOOKS_PER_PAGE);
 optionsRenderer.render(genres, "[data-search-genres]", "All Genres");
 optionsRenderer.render(authors, "[data-search-authors]", "All Authors");
-
-
